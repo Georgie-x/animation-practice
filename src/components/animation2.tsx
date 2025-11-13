65570d3904486fc4e04431807b3ef6dedc4f3702
Georgie <georgiex@gmail.com>
2025-11-13 23:00:48 +0000

    more messing

--- src/components/animation2.tsx
+++ src/components/animation2.tsx
@@ -1,6 +1,12 @@
 function Animation2() {
 	return (
-		<div className="animation"><h1 className="text-fade">Georgie</h1></div>
+		<div className='animation'>
+			<h2 className='oneSec'>out of sync</h2>
+			<h2 className='twoSec'>out of sync</h2>
+			<h2 className='fourSec'>out of sync</h2>
+			<h2 className='eightSec'>out of sync</h2>
+			<h2 className='sixteenSec'>out of sync</h2>
+		</div>
 	)
 }
 
--- src/components/animation3.tsx
+++ src/components/animation3.tsx
@@ -1,7 +1,14 @@
 function Animation3() {
-	return (
-		<div className="animation"></div>
-	)
+  return (
+    <div className="animation parallax">
+      <div className="snowflake-far"></div>
+      <div className="snowflake-mid"></div>
+      <div className="snowflake-near"></div>
+      <div className="building">
+       
+      </div>
+    </div>
+  );
 }
 
-export default Animation3
+export default Animation3;
